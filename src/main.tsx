5c9a25cef46d3cededeca2c7208cbcf720ead19a
s.chechet <chechet@mail.ru>
2025-04-29 10:42:17 +0400

    global styles added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './store'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './styles/global.scss'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
