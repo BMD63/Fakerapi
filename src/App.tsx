f2c2bbd1136e265fcd25b66c1f79b4324b4190cd
s.chechet <chechet@mail.ru>
2025-04-30 15:31:31 +0400

    App styled

--- src/App.tsx
+++ src/App.tsx
@@ -5,36 +5,42 @@ import LoginPage from './components/LoginPage';
 import PrivateRoute from './components/PrivateRoute'; // Импортируем PrivateRoute
 import Feedback from './components/Feedback'; // Импортируем Feedback
 import PersonalAccount from './components/PersonalAccount'; 
+import styles from './App.module.scss'; 
 
 function App() {
   return (
-    
     <Router>
-        <nav>
-          <ul>
-            <li>
+      <div className={styles.appContainer}>
+        <nav className={styles.mainNav}>
+          <ul className={styles.navList}>
+            <li className={styles.navItem}>
               <Link to="/">Главная</Link>
             </li>
-            <li>
+            <li className={styles.navItem}>
               <Link to="/login">Войти</Link>
             </li>
-            <li>
+            <li className={styles.navItem}>
               <Link to="/feedback">Обратная связь</Link>
             </li>
-            <li>
+            <li className={styles.navItem}>
               <Link to="/personal-account">Личный кабинет</Link>
             </li>
           </ul>
         </nav>
-
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/:id" element={<ProductDetailPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/feedback" element={<Feedback />} />
-          <Route path="/personal-account" element={<PrivateRoute><PersonalAccount /></PrivateRoute>} />          <Route path="*" element={<h1>404 Not Found</h1>} /> Добавляем обработку несуществующих маршрутов
-        </Routes>
+        <div className={styles.content}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/:id" element={<ProductDetailPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/feedback" element={<Feedback />} />
+            <Route path="/personal-account" element={<PrivateRoute><PersonalAccount /></PrivateRoute>} />          <Route path="*" element={<h1>404 Not Found</h1>} /> Добавляем обработку несуществующих маршрутов
+          </Routes>
+          </div>
+        </div>
     </Router>
+    
+
+
   );
 }
 
